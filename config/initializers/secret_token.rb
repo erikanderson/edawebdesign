2c9c4db4bbce0f96f4103c61d11fd9b141d42704
Erik <edawebdesign@gmail.com>
2014-01-30 22:35:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edawebdesign::Application.config.secret_key_base = '479e4ef6dcf53af9d8be6197bcc1606e9f360fd2f5953b3714848e59cc74dff6ac5c35b26c5ba0de0307f8c2826d9a8eea94b7a2e7f2a924da7e3f4c4a537109'
