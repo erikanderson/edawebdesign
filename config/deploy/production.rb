f7ec0aea0d9d034581901d8432a91476cdc33038
Erik <edawebdesign@gmail.com>
2014-02-16 16:48:58 -0500

    prep for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,6 +16,7 @@ role :db,  %w{deploy@162.243.102.243}
 # extended properties on the server.
 server '162.243.102.243', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
+
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
