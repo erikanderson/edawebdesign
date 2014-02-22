f76a01bd1c941117ea0eab78b5546f461e4172b7
Erik <edawebdesign@gmail.com>
2014-02-22 15:39:55 -0500

    portfolio and awards

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Edawebdesign::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :warn
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -67,7 +67,7 @@ Edawebdesign::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
+  #config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
